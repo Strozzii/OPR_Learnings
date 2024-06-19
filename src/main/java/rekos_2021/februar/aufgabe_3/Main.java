d37475e1932f3d335842aaee579f62e5f6e7b3a2
Strozzii <stroz.dennis@gmail.com>
2024-06-19 09:35:22 +0200

    Added Februar 2021 Aufgabe 3

---
+++ src/main/java/rekos_2021/februar/aufgabe_3/Main.java
@@ -0,0 +1,41 @@
+package rekos_2021.februar.aufgabe_3;
+
+import java.util.*;
+
+public class Main {
+
+    public static Map<Character, Collection<String>> erzeugeIndex(String[] woerter){
+        Map<Character, Collection<String>> ausgabe = new HashMap<>();
+
+        for (String s : woerter) {
+            char erstes_zeichen = s.charAt(0);
+
+            if (ausgabe.containsKey(erstes_zeichen)) {
+                ausgabe.get(erstes_zeichen).add(s);
+            }
+            else {
+                Collection<String> liste = new HashSet<>();
+                liste.add(s);
+
+                ausgabe.put(erstes_zeichen, liste);
+            }
+
+        }
+
+        return ausgabe;
+    }
+
+    public static void main(String[] args) {
+        String[] woerter = {
+                "Fahrrad",
+                "123 Abflussreinigung",
+                "Fahrstuhl",
+                "frische Blumen",
+                "Fahrrad",
+                "Pfirsich"
+        };
+
+        System.out.println(erzeugeIndex(woerter));
+    }
+
+}

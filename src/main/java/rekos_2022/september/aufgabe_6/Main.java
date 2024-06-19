c42f9c53f667e7fcf3217280fa18759cd580ce5d
Strozzii <stroz.dennis@gmail.com>
2024-06-20 00:33:41 +0200

    2022 Reko Aufgabe 6

---
+++ src/main/java/rekos_2022/september/aufgabe_6/Main.java
@@ -0,0 +1,14 @@
+package rekos_2022.september.aufgabe_6;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 1)
+        BufferedReader br = new BufferedReader(new StringReader("ab ab ab ab ab"));
+
+
+    }
+}

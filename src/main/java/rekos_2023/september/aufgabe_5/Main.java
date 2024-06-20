16aaced003180f4eeac1914633eedeea7da28a41
Strozzii <stroz.dennis@gmail.com>
2024-06-20 11:39:11 +0200

    2023 Reko Aufgabe 5

---
+++ src/main/java/rekos_2023/september/aufgabe_5/Main.java
@@ -0,0 +1,22 @@
+package rekos_2023.september.aufgabe_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vogelzaehlung v = new Vogelzaehlung();
+
+        v.vogel("B_Vogel", 3);
+        v.vogel("E_Vogel", 2);
+        v.vogel("A_Vogel", 1);
+        v.vogel("E_Vogel", 5);
+        v.vogel("C_Vogel", 8);
+
+        System.out.println(v.alleVoegel());
+        System.out.println(v.anzahl("E_Vogel"));
+        System.out.println(v.anzahl("Kein_Vogel"));
+
+
+
+    }
+
+}

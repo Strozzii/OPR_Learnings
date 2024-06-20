87af8eeaa5647a28792868d3f668d09f6b1b21be
Strozzii <stroz.dennis@gmail.com>
2024-06-20 14:00:17 +0200

    2023 Reko Aufgabe 11

---
+++ src/main/java/rekos_2023/september/aufgabe_11/Paket.java
@@ -0,0 +1,39 @@
+package rekos_2023.september.aufgabe_11;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+
+public enum Paket {
+
+    S(2),
+    M(5),
+    L(10),
+    XL(25);
+
+    private int gewicht_grenze;
+
+    Paket(int gewicht_grenze) {
+        this.gewicht_grenze = gewicht_grenze;
+    }
+
+    public int maxGewicht() {
+        return this.gewicht_grenze;
+    }
+
+    public Optional<Paket> paket(double gewicht) {
+        return Arrays.stream(Paket.values())
+                .filter(p -> p.maxGewicht() >= gewicht)
+                .findFirst();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(S.paket(18));
+        System.out.println(S.paket(28));
+    }
+
+
+
+
+}
